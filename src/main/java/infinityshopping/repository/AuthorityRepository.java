54c740db49d4eed1a18c45959a4b57ec6fcb365c
kosovic-drasko <kosovic.drasko@montefarmco.me>
2023-03-28 09:52:22 +0200

    Initial version of shop generated by generator-jhipster@7.9.3

---
+++ src/main/java/infinityshopping/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package infinityshopping.repository;
+
+import infinityshopping.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/infinityshopping/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package infinityshopping.security;
+
+import infinityshopping.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
