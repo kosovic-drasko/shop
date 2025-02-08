ac538e7be0388d526b53d0127d07e9ffbcc34ad6
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2025-02-08 22:33:32 +0100

    DetailPage

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,11 +4,20 @@ import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get latest products
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   });
 
+
+
   return convertToPlainObject(data);
+
+
 }
